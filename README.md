29ad003f9f58f906394de9f3ae1911d7534b2121
Yurii Zhurakovskyi <yurii_zhurakovskyi@epam.com>
2020-03-04 14:02:15 +0200

    Provided solution

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # AllPermutations
+
+Steps:
+1. Convert long number to the vector of digits
+2. Find all permutations of the longest sequence
+3. Rotate vector step by step
+    3.1 Find all permutations of the shortest subvectors
+4. Reduce number of overhead calculations by providing of global set of calculated subsequences.
+5. Obtained results:
+    /*
+    Digits  | Number of permutations
+      1     |       1
+      2     |       4
+      3     |       15
+      4     |       60
+      5     |       285
+      6     |       1638
+      7     |       11151
+      8     |       87624
+      9     |       778977
+      10    |       6901705
+    */
--- main.cpp
+++ main.cpp
@@ -1,9 +1,80 @@
 #include <iostream>
+#include <vector>
+#include <unordered_set>
+#include <algorithm>
 
 using namespace std;
 
+std::unordered_set<uint64_t> used;
+
+std::vector<uint32_t> split_long(uint64_t n)
+{
+    std::vector<uint32_t> res;
+    while(n)
+    {
+        res.emplace_back(uint32_t(n % 10));
+        n /= 10;
+    }
+    return res;
+}
+
+uint64_t restore_long(const std::vector<uint32_t>& split)
+{
+    uint64_t mult = 1;
+    uint64_t res = 0;
+
+    for (auto it = split.crbegin(); it != split.crend(); ++it)
+    {
+        res += *it * mult;
+        mult *= 10;
+    }
+
+    return res;
+}
+
+void permutate(std::vector<uint32_t> split, std::unordered_set<uint64_t>& permutations)
+{
+    std::sort(split.begin(), split.end());
+    uint64_t test = restore_long(split);
+    auto it = used.find(test);
+    if (it == used.end())
+    {
+        used.emplace(test);
+        permutations.emplace(restore_long(split));
+        while(std::next_permutation(split.begin(), split.end()))
+        {
+            permutations.emplace(restore_long(split));
+        }
+    }
+}
+
 int main()
 {
-    cout << "Hello" << endl;
+    uint64_t n = 123456789;   
+    std::vector<uint32_t> split = split_long(n);
+
+    std::unordered_set<uint64_t> permutations{};
+    permutate(split, permutations);
+    
+    for (auto it = split.begin(); it != split.end(); ++it)
+    {
+        std::vector<uint32_t> rotated_split(split.size());
+        std::rotate_copy(split.begin(), it, split.end(), rotated_split.begin());
+        for (auto i = rotated_split.begin() + 1; i != rotated_split.end(); ++i)
+        {
+            permutate(std::vector<uint32_t>(i, rotated_split.end()), permutations);
+        }
+    }
+
+    // Print all of the permutations if needed:
+    /*
+    for (auto p : permutations)
+    {
+        cout << p << endl;
+    }
+    */
+    
+    cout << "Number of permutations: " << permutations.size() << endl;
+
     return 0;
 }
